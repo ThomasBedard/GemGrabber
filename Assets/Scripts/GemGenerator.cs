4df9aca15c66744af67804933323879acd70a09c
HennCH <2132132@champlaincollege.qc.ca>
2023-05-14 21:05:23 -0400

    Gem generator
    I am adjusting gem generator and modifying the scenes

--- Assets/Scripts/GemGenerator.cs
+++ Assets/Scripts/GemGenerator.cs
@@ -9,6 +9,12 @@ public class GemGenerator : MonoBehaviour
 
     [SerializeField]
     private GameObject gem;
+
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    private GameObject camera;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +26,27 @@ public class GemGenerator : MonoBehaviour
         
         Vector2 position;
 
-        position = new Vector2(Random.Range(-16,20),Random.Range(-11,9));
+       position = new Vector2(Random.Range(-16,20),Random.Range(-11,9));
+
+        for (int i = 0; i < 1; i++)
+        {
+            if (gem.transform.position.x == player.transform.position.x)
+            {
+
+                position = new Vector2(Random.Range(-16, 20), Random.Range(-11, 9));
+
+                if(position.x <= camera.transform.position.x && position.y <= camera.transform.position.y)
+                {
+                    position = new Vector2(Random.Range(-14, -27), Random.Range(0, 2));
+                }
+            }
+            else
+            {
+                position = new Vector2(Random.Range(-14, -27), Random.Range(0, 2));
+                i++;
+            }
+            
+        }
 
         Instantiate(gem, position, Quaternion.identity);
     }
