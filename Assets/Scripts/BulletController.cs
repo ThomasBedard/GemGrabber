eec7257146c727d77a73c04c67906d0afc4335b2
Thomas <thomas.bedard0@gmail.com>
2023-05-14 22:52:28 -0400

    Player shooting, Enemy spawning & they can be eliminated
    combat changes to the game

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+
+    public float speed = 10f;
+    public delegate void BulletDestroyed();
+    public event BulletDestroyed OnBulletDestroyed;
+    public float lifetime = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.up * speed * Time.deltaTime;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // If the bullet collides with an enemy, raise the OnBulletDestroyed event
+        if (other.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+            // Call the OnBulletDestroyed event
+            OnBulletDestroyed?.Invoke();
+        }
+    }
+    void OnDestroy()
+    {
+        // Trigger the OnBulletDestroyed event when the bullet is destroyed
+        OnBulletDestroyed?.Invoke();
+    }
+}
---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] int maxBullets = 3;
+    [SerializeField] float bulletSpeed = 10f;
+    private int currentBullets = 0;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && currentBullets < maxBullets)
+        {
+            FireBullet();
+        }
+
+        // Rotate the empty object towards the mouse cursor
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
+    }
+
+    void FireBullet()
+    {
+        // Instantiate the bullet object
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+
+        // Calculate the direction vector between the player's position and the mouse position
+        Vector2 direction = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
+
+        // Set the velocity of the bullet according to the direction vector
+        bullet.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
+
+        // Ignore collisions with the character's layer
+        int characterLayer = LayerMask.NameToLayer("Character");
+        Physics2D.IgnoreLayerCollision(gameObject.layer, characterLayer, true);
+
+        // Increment the bullet count and add an event listener to the bullet's OnBulletDestroyed event
+        currentBullets++;
+        bullet.GetComponent<BulletController>().OnBulletDestroyed += OnBulletDestroyed;
+
+        // Reset layer collision ignore after a short delay
+        StartCoroutine(ResetLayerCollisionIgnore(characterLayer));
+    }
+
+    IEnumerator ResetLayerCollisionIgnore(int layer)
+    {
+        yield return new WaitForSeconds(0.1f);
+        Physics2D.IgnoreLayerCollision(gameObject.layer, layer, false);
+    }
+
+    void OnBulletDestroyed()
+    {
+        currentBullets--;
+    }
+}
